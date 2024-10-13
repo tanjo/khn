61c9a705f6c29ca4a2eafb2c5837dae77b301d9d
tanjo <tanjo@users.noreply.github.com>
2024-10-13 23:48:57 +0900

    Ver. 0.2.0

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export class Constants {
-    public static YAHOO_URL = 'https://www.yahoo.co.jp/';
-    public static GOOGLE_URL = 'https://www.google.com/';
-    public static BING_URL = 'https://www.bing.com/';
-
-    public static URL_REGEX = /^https?:\/\/.+/;
-}
